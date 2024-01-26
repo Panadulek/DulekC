74ab660c2ddaf9b3c2f265a538da32153790db6a
jordan <jazborj@gmail.com>
2024-01-26 20:48:25 +0100

    preaper_to_changes

--- DulekC/FlagsSettings.h
+++ DulekC/FlagsSettings.h
@@ -1,83 +1,20 @@
 #pragma once
 #include <string>
 #include <iostream>
-class CompilerCommunicate
-{
-	static const std::string  FunctionInFunction()
-	{
-		return "Not allowed to declare Function in Function";
-	}
-	CompilerCommunicate() {}
-	friend class CompilerParserFlags;
-};
 
-class CompilerParserFlags
+
+
+
+class LexerManager
 {
 
-	bool m_inFunction;
-	bool m_inStruct;
-	bool m_inGlobal;
-	int m_braceCounter;
-	int m_buckleCounter;
-
-	void err(const std::string& str)
+	enum class Context
 	{
-		std::cout << str << std::endl;
-	}
-public:
-	static CompilerParserFlags& instance() 
-	{
-		static CompilerParserFlags _cpf;
-		return _cpf;
-	}
+		GLOBAL,
+		FUNCTION
+	};
 
-	CompilerParserFlags() : m_inFunction(false), m_inStruct(false), m_inGlobal(false)
-	{}
 
-	bool enterFunction()
-	{
-		if (m_inFunction)
-		{
-			err(CompilerCommunicate::FunctionInFunction());
-			return false;
-		}
 
-		m_inFunction = true;
-		m_inGlobal = false;
-		return true;
-	}
 
-	void exitFunction()
-	{
-		m_inFunction = false;
-		m_inGlobal = true;
-	}
-
-	bool isInFunction()
-	{
-		return (m_inFunction && !m_inGlobal);
-	}
-
-	int openBrace()
-	{
-		m_braceCounter++;
-		return m_braceCounter;
-	}
-	int closeBrace()
-	{
-		m_braceCounter--;
-		return m_braceCounter;
-	}
-
-	int openBuckle()
-	{
-		m_buckleCounter++;
-		return m_buckleCounter;
-	}
-
-	int closeBuckle()
-	{
-		m_buckleCounter--;
-		return m_buckleCounter;
-	}
 };
