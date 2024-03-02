dc880c6739d250443b8647680de99eedeaaffc7f
jordan <jazborj@gmail.com>
2024-03-02 21:22:46 +0100

    add project where will be system function implemented in c

---
+++ CFunctions/DuFunctions.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Wyklucz rzadko używane rzeczy z nagłówków systemu Windows
+#include <cinttypes>
+extern "C"
+{
+	void* DuAllocate(uint64_t size);
+
+	void* DuReallocate(void* memory, uint64_t size);
+
+	void DuDeallocate(void* memory);
+
+
+	int DuDisplay(const char* fmt, ...);
+}
