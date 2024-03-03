1731e1c768d5063a751a2a308f78518807fdc716
jordan <jazborj@gmail.com>
2024-03-03 23:14:38 +0100

    add SystemFunctions.cpp file

---
+++ DulekC/SystemFunctions.cpp
@@ -0,0 +1,32 @@
+#include "SystemFunctions.h"	
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Verifier.h>
+#include "DuFunctions.h"
+void SystemFunctions::generatePrintNumberFunction()
+{
+	llvm::FunctionType* printFunctionType = llvm::FunctionType::get(m_builder->getInt32Ty(), m_builder->getInt32Ty(), false);
+	auto functionPtr = llvm::Function::Create(printFunctionType, llvm::Function::LinkageTypes::ExternalLinkage, "DuDisplayNumber", m_module);
+	auto printfFunc = llvm::FunctionCallee(functionPtr);
+	m_functions.insert({ getSysFunctionName<SysFunctionID::DISPLAY>(), printfFunc });
+}
+void SystemFunctions::generateAllocateFunction()
+{
+	llvm::FunctionType* allocateFunctionType = llvm::FunctionType::get(m_builder->getVoidTy()->getPointerTo(), m_builder->getInt64Ty(), false);
+	auto functionPtr = llvm::Function::Create(allocateFunctionType, llvm::Function::LinkageTypes::ExternalLinkage, "DuAllocate", m_module);
+	auto allocateFunc = llvm::FunctionCallee(functionPtr);
+	m_functions.insert({ getSysFunctionName<SysFunctionID::ALLOCATE_MEMORY>(), allocateFunc });
+}
+void generateDeallocateFunction()
+{
+
+}
+
+llvm::FunctionCallee* SystemFunctions::findFunction(Identifier id)
+{
+	auto it = m_functions.find(id.getName().data());
+	if (m_functions.end() == it)
+	{
+		return nullptr;
+	}
+	return &it->second;
+}
